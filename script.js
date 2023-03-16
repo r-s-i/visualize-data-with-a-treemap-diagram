e4bcb97566acf7ab0627c5269f08a621ec72e9c1
r-s-i <77388911+r-s-i@users.noreply.github.com>
2023-03-16 15:28:42 +0100

    Added tooltip + formating, 12 / 12 tests passed

--- script.js
+++ script.js
@@ -7,10 +7,7 @@ const svg = d3
   .attr("width", width)
   .attr("height", height);
 
-const colors = [
-  "red", "orange", "yellow", "green", 
-  "blue", "indigo", "violet"
-];
+const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
 
 fetch(
   "https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/movie-data.json"
@@ -25,8 +22,8 @@ fetch(
       .attr("name", (d) => d.name)
       .attr("id", (d) => d.name.toLowerCase());
 
-      let currentCategory = "";
-      let yValue = 0;
+    let currentCategory = "";
+    let yValue = 0;
     d.children.forEach((e, i) => {
       d3.select(`#${e.name.toLowerCase()}`)
         .selectAll("rect")
@@ -41,21 +38,36 @@ fetch(
         .attr("height", 10)
         .attr("x", (e, i) => {
           if (currentCategory !== e.category) {
-            currentCategory = e.category
+            currentCategory = e.category;
             yValue += 10;
             return 0;
-          }
-          else {
-            return i * width * 0.01
+          } else {
+            return i * width * 0.01;
           }
         })
         .attr("y", yValue)
-
-        .style("fill", colors[i]);
+        .style("fill", colors[i])
+        .on("mouseover", (e, d) => {
+          const tooltip = svg
+            .append("foreignObject")
+            .html(`${d.name}\n${d.category}\n$${d.value}`)
+            .attr("id", "tooltip")
+            .attr("data-value", d.value);
+        })
+        .on("mouseout", (e, d) => {
+          const tooltip = d3.select("#tooltip");
+          tooltip.remove();
+        });
     });
 
-    const legend = svg.append("g").attr("id", "legend")
+    const legend = svg.append("g").attr("id", "legend");
 
-    const legendItems = legend.selectAll("rect").data(d.children).enter()
-    .append("rect").attr("class", "legend-item").attr("name", (d) => d.name).style("fill", (_, i) => colors[i])
+    const legendItems = legend
+      .selectAll("rect")
+      .data(d.children)
+      .enter()
+      .append("rect")
+      .attr("class", "legend-item")
+      .attr("name", (d) => d.name)
+      .style("fill", (_, i) => colors[i]);
   });
